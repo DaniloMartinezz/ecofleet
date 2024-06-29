37f00875e1208eb7e8caf55141c7a6745b3b108a
DaniloMartinezz <danilomartinez.dev@gmail.com>
2024-06-29 11:45:13 -0300

    finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from './context/ThemeContext';
 import AppRoutes from "./Routes";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App() {
   return (
     <ThemeProvider>
-      <AppRoutes/>
+      <SpeedInsights />
+      <AppRoutes />
     </ThemeProvider>
   );
 }
